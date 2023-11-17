eca76852fd3677ee6b8f265a555aa74478ce4fca
Tonya Kazlovskaya <toshichka.91@gmail.com>
2023-11-17 17:35:33 +0300

    added homework_13 task 1

--- src/main/java/pl/pingwit/homework_13/person/PersonParser.java
+++ src/main/java/pl/pingwit/homework_13/person/PersonParser.java
@@ -4,33 +4,32 @@ import java.time.DateTimeException;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
 
 public class PersonParser {
-    public Person[] people(String text) {
+    public Person[] personParser(String text) {
         String[] item = text.split(";");
-        Person[] people = new Person[item.length];
+        Person[] splitOfPerson = new Person[item.length];
         for (int i = 0; i < item.length; i++) {
-            people[i] = pars(item[i]);
+            splitOfPerson[i] = itemParser(item[i]);
         }
-        return people;
+        return splitOfPerson;
     }
 
-    private Person pars(String text) {
+    private Person itemParser(String text) {
 
-        String[] g = text.split(",");
-        String n = g[0].trim();
-        String su = g[1].trim();
+        String[] strings = text.split(",");
+        String name = strings[0].trim();
+        String surname = strings[1].trim();
 
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-dd-MM");
-        LocalDate b = LocalDate.parse(g[3].trim(), dateTimeFormatter);
-        DayOfWeek d=null;
+        LocalDate dayOfBirth = LocalDate.parse(strings[3].trim(), dateTimeFormatter);
+        DayOfWeek dayOfWeek=null;
 
        try {
-            d = DayOfWeek.of(Integer.parseInt(g[2].trim()));
+            dayOfWeek = DayOfWeek.of(Integer.parseInt(strings[2].trim()));
        }catch (DateTimeException e){
            System.out.println(e.getMessage());
        }
-        return new Person (n,su,b,d);
+        return new Person (name,surname,dayOfBirth,dayOfWeek);
     }
 }
